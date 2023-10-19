162fb5d76759e919b20a69d79818010f90adb07c
cp <iamdchinh@gmail.com>
2023-10-19 22:37:26 +0700

    add eventbridge scheudler execution role

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+# IAM for lambda fucntion
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_functions_iam_role_for_slack_dnd"
   assume_role_policy = jsonencode(
@@ -24,4 +25,54 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
 resource "aws_iam_role_policy_attachment" "lambda_ssm_read_only" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+
+# IAM for EventBridge Scheduler
+resource "aws_iam_role" "eventbridge_execution_role" {
+  name = "Amazon_EventBridge_Scheduler_LAMBDA_dawc"
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "scheduler.amazonaws.com"
+          },
+          "Action" : "sts:AssumeRole",
+          "Condition" : {
+            "StringEquals" : {
+              "aws:SourceAccount" : "434545458459"
+            }
+          }
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_policy" "eventbridge_scheduler_execution" {
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "lambda:InvokeFunction"
+          ],
+          "Resource" : [
+            aws_lambda_function.lambda.arn
+          ]
+
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "exec_lambda_function" {
+  role       = aws_iam_role.eventbridge_execution_role.name
+  policy_arn = aws_iam_policy.eventbridge_scheduler_execution.arn
 }
